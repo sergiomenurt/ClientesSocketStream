564059d70a0e25ea5381b4df9139c88d0ac78868
sergiomenurt <smendezurtaza@danielcastelao.org>
2023-02-24 10:05:29 +0100

    Initial commit

---
+++ src/ClientesSocketStream.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class ClientesSocketStream {
+
+    public static void main(String[] args){
+        try{
+            System.out.println("Creando socket cliente");
+            Socket clienteSocket=new Socket();
+            System.out.println("Estableciendo la conexion");
+
+            InetSocketAddress addr=new InetSocketAddress("localhost",5555);
+            clienteSocket.connect(addr);
+
+            InputStream is = clienteSocket.getInputStream();
+            OutputStream os= clienteSocket.getOutputStream();
+
+            System.out.println("Enviando mensaje");
+
+            String mensaje="mensaje desde el cliente";
+            os.write(mensaje.getBytes());
+
+            System.out.println("Mensaje enviado");
+
+            System.out.println("Cerrando el socket cliente");
+
+            clienteSocket.close();
+
+            System.out.println("Terminado");
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}	
